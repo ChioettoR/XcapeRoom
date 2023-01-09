5a99e1c82413e78a54e32279e3b5237a46ec6696
Riccardo <riccardo.chioetto@yahoo.com>
2023-01-09 17:42:16 +0100

    Palm mechanism completed + goblet fill + map sprites

---
+++ Assets/MyAssets/Scripts/PalmRiddle/GobletFiller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+public class GobletFiller : MonoBehaviour
+{
+    public Renderer rend;
+    public Interactable gobletInteractable;
+    public float fullFillAmount;
+    public float emptyFillAmount;
+    public float fullTime;
+
+    void Start()
+    {
+        rend.material.SetFloat("_FillAmount", emptyFillAmount);
+    }
+
+
+    public void Fill()
+    {
+        StartCoroutine(FillCoroutine());
+    }
+
+    IEnumerator FillCoroutine()
+    {
+        gobletInteractable.enabled = false;
+        float time = 0;
+        while (time < fullTime)
+        {
+            time += Time.deltaTime;
+            rend.material.SetFloat("_FillAmount", Mathf.Lerp(emptyFillAmount, fullFillAmount, time / fullTime));
+            yield return null;
+        }
+        gobletInteractable.enabled = true;
+    }
+}
---
+++ Assets/MyAssets/Scripts/PalmRiddle/PalmRiddleManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+public class PalmRiddleManager : MonoBehaviour
+{
+    public List<Interactable> collectableObjects;
+    public GobletFiller gobletFiller;
+    public GameObject gobletDockZone;
+    public List<ParticleSystem> waterDripParticles;
+
+    private int correctObjectsPlaced;
+    private bool gobletPlaced = false;
+    private bool gobletFilled = false;
+
+    public void CorrectObjectPlaced()
+    {
+        correctObjectsPlaced++;
+
+        if (correctObjectsPlaced == 2)
+        {
+            foreach (Interactable interactable in collectableObjects) interactable.enabled = false;
+            ActiveFountain();
+        }
+    }
+
+    public void CorrectObjectRemoved()
+    {
+        correctObjectsPlaced--;
+    }
+
+    public void GobletPlaced()
+    {
+        gobletPlaced = true;
+        if (correctObjectsPlaced == 2)
+        {
+            gobletFiller.Fill();
+            gobletFilled = true;
+        }
+    }
+
+    public void GobletRemoved()
+    {
+        gobletPlaced = false;
+
+        if(gobletFilled) gobletDockZone.SetActive(false);
+    }
+
+    private void ActiveFountain()
+    {
+        foreach (ParticleSystem particleSystem in waterDripParticles) particleSystem.Play();
+        if (gobletPlaced) 
+        {
+            gobletFiller.Fill();
+            gobletFilled = true;
+        }
+    }
+}
