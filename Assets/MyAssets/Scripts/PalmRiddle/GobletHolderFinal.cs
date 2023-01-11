d9540e29226b89c548896c0e7f15e882e135c6db
Riccardo <riccardo.chioetto@yahoo.com>
2023-01-11 16:02:35 +0100

    Third riddle improvements

--- Assets/MyAssets/Scripts/PalmRiddle/GobletHolderFinal.cs
+++ Assets/MyAssets/Scripts/PalmRiddle/GobletHolderFinal.cs
@@ -8,13 +8,29 @@ public class GobletHolderFinal : MonoBehaviour
     public Animator drawerAC;
     public GobletFiller gobletFiller;
     public Interactable gobletInteractable;
+    public Interactable gobletStandInteractable;
 
     public void GobletInserted()
     {
-        if(gobletFiller.CheckFilled())
-        {
-            gobletInteractable.enabled = false;
-            drawerAC.SetTrigger("OpenDrawer");
-        }
+        gobletInteractable.enabled = false;
+
+        if (gobletFiller.CheckFilled()) drawerAC.SetTrigger("OpenDrawer");
+        else drawerAC.SetTrigger("GobletStandDown");
+    }
+
+    public void GobletRemoved()
+    {
+        gobletStandInteractable.enabled = false;
+        drawerAC.SetTrigger("GobletStandUp");
+    }
+
+    public void GobletDownAnimationFinished()
+    {
+        gobletInteractable.enabled = true;
+    }
+
+    public void GobletUpAnimationFinished()
+    {
+        gobletStandInteractable.enabled = true;
     }
 }
